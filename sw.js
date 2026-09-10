deebc24a33e21478c7daf7d30cd1db49c9d35469
Charuth <thameeshacharuth@gmail.com>
2026-09-10 20:28:42 +0530

    upgrade version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'n5-quiz-v2';
-const FILES = ['/japanese-n5-quiz/index.html', '/japanese-n5-quiz/index2.html'];
+const FILES = ['/japanese-n5-quiz/index.html'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
